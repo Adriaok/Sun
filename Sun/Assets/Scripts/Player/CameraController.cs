8e89f8b575200aaaf95f2468c88978205607aba6
Resin <99646066+Adriaok@users.noreply.github.com>
2023-11-07 21:24:27 +0100

    camera setup

--- Sun/Assets/Scripts/Player/CameraController.cs
+++ Sun/Assets/Scripts/Player/CameraController.cs
@@ -1,29 +1,30 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
-    [SerializeField] public PlayerController player;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 45.0f;
-
-    Vector2 rotation = Vector2.zero;
+    [SerializeField] CinemachineVirtualCamera _virtualCamera;
 
     void Start()
     {
-        player = GetComponentInParent<PlayerController>();
-        rotation.y = transform.eulerAngles.y;
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
-        rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
-        rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
-        transform.localRotation = Quaternion.Euler(rotation.x, 0, 0);
-        player.transform.eulerAngles = new Vector2(0, rotation.y);
+        //TODO: when player cursor moves in a direction camera damps that way
+        //TODO: try to contain all followers in camera 
+        //TODO: righ click + mouse to move
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Input.GetAxis("Mouse X");
+            Input.GetAxis("Mouse Y");
+        }
+
+
     }
 }
