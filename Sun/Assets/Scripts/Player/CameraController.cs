bd88c38a44f964290503e8c3f3f007def2763464
Resin <99646066+Adriaok@users.noreply.github.com>
2023-11-08 12:10:58 +0100

    wowza

--- Sun/Assets/Scripts/Player/CameraController.cs
+++ Sun/Assets/Scripts/Player/CameraController.cs
@@ -15,12 +15,15 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         //TODO: when player cursor moves in a direction camera damps that way
         //TODO: try to contain all followers in camera 
         //TODO: righ click + mouse to move
 
         if (Input.GetMouseButtonDown(1))
         {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                
             Input.GetAxis("Mouse X");
             Input.GetAxis("Mouse Y");
         }
