cbb1c37976de08eec6f732c441f335b0e3ff9099
Resin <99646066+Adriaok@users.noreply.github.com>
2023-11-09 13:53:51 +0100

    completed

--- Sun/Assets/Scripts/Player/CameraController.cs
+++ Sun/Assets/Scripts/Player/CameraController.cs
@@ -2,42 +2,45 @@ using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
+using static Cinemachine.CinemachineFreeLook;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera _virtualCamera;
-    [SerializeField] float rotationSpeed;
+    CinemachineOrbitalTransposer _transposer;
+    private bool canTurn;
 
 
     void Start()
     {
         _virtualCamera = GetComponent<CinemachineVirtualCamera>();
-        rotationSpeed = 100f;
+        _transposer = _virtualCamera.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+        _transposer.m_XAxis.m_InputAxisName = null;
+        canTurn = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("space"))
+            canTurn = true;
 
-
-        //TODO: when player cursor moves in a direction camera damps that way
-        //TODO: try to contain all followers in camera 
-        //TODO: righ click + mouse to move
-
-        if (Input.GetMouseButtonDown(1))
+        if (canTurn)
         {
-            float rotY = rotationSpeed * Input.GetAxis("Mouse X");
-            float rotZ = rotationSpeed * Input.GetAxis("Mouse Y");
-            //transform.rotation = Quaternion.AngleAxis(0f, 0f, 0f);
-
-            transform.rotation = Quaternion.Euler(40f, rotY, rotZ);
-
+            if (_transposer.m_XAxis.Value != 0)
+                _transposer.m_RecenterToTargetHeading.m_enabled = true;
+            else
+            {
+                _transposer.m_RecenterToTargetHeading.m_enabled = false;
+                canTurn = false;
+            }
         }
-        Debug.Log(Input.GetAxis("Mouse X"));
-        //Console.WriteLine();
-        //Console.WriteLine(Input.GetAxis("Mouse Y"));
-        //Console.WriteLine(Input.GetAxis("Mouse Y"));
 
+        if (Input.GetMouseButton(1))
+            _transposer.m_XAxis.m_InputAxisName = "Mouse X";
+        else
+            _transposer.m_XAxis.m_InputAxisName = null;
     }
 }
