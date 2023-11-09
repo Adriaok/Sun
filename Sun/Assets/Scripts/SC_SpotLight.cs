5a5d8741fb4fd335faea726ba0b028c6f296c08f
IrisLazaroCanet <99645778+IrisLazaroCanet@users.noreply.github.com>
2023-11-09 13:34:17 +0100

    Test light by code
    Paràmetre sortida (out...) .-> estructura RAYCAST HIT que conté una referència a l'objecte a què ha donat el raycast

--- Sun/Assets/Scripts/SC_SpotLight.cs
+++ Sun/Assets/Scripts/SC_SpotLight.cs
@@ -3,31 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 
-//https://docs.unity3d.com/ScriptReference/Experimental.GlobalIllumination.SpotLight.html
-//https://docs.unity3d.com/ScriptReference/Experimental.GlobalIllumination.LinearColor.html
-//https://catlikecoding.com/unity/tutorials/custom-srp/point-and-spot-lights/
+//UnityEngineLight
+//NO FER SERVIR EXPERIMENTAL
+
+//Raycast de spot light per detecar si quelcom està il·luminat (llargada)
 
 public class SC_SpotLight : MonoBehaviour
 {
-    private SpotLight light; 
     
     // Start is called before the first frame update
     void Start()
     {
-        light.angularFalloff = AngularFalloffType.LUT;
-        light.color = LinearColor.Convert(Color.white, 1.0f);
-        light.coneAngle = 45.0f;
-        light.falloff = FalloffType.Linear;
-        light.indirectColor = LinearColor.Convert(Color.white, 1.0f);
-        light.innerConeAngle = 30.0f;
-        //TODO: Refactor follower id to int? 
-        //light.instanceID =  GetComponent<SC_Follower>().ID;
-        light.mode = LightMode.Realtime;
-        light.orientation = GetComponent<SC_Follower>().transform.rotation;
-        light.position = GetComponent<SC_Follower>().transform.position;
-        light.range = GetComponent<SC_Follower>().lightRange;
-        light.shadow = true;
-        light.sphereRadius = 5.0f;  //TODO: Check if this value makes sense
+        GetComponentInParent<Light>().type = UnityEngine.LightType.Spot;
+
+        GetComponentInParent<Light>().color = Color.green;
+        Debug.Log("Color changed to green");
     }
 
     // Update is called once per frame
