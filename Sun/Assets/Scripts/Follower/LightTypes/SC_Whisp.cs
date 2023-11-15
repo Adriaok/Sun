dfefa5c11326e5dc0bd317795cb5d2ce2122af59
IrisLazaroCanet <99645778+IrisLazaroCanet@users.noreply.github.com>
2023-11-15 13:27:58 +0100

    Whisp and Flashlight basic code

---
+++ Sun/Assets/Scripts/Follower/LightTypes/SC_Whisp.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class SC_Whisp : MonoBehaviour
+{
+    private Light light;
+    private LightUpObject foundObject;
+    private string raycastReturn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponentInParent<Light>();
+
+        GetComponentInParent<Light>().type = UnityEngine.LightType.Spot;
+        GetComponentInParent<Light>().color = Color.red;
+        GetComponentInParent<Light>().intensity = 1.0f;
+        GetComponentInParent<Light>().range = 500.0f;
+    }
+
+    private void FixedUpdate()
+    {
+        CheckRayCastCollision();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void CheckRayCastCollision()
+    {
+        // Bit shift the index of the layer (6) to get a bit mask
+        // This would cast rays only against colliders in layer 6.
+        int layerMask = 1 << 6;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(GetComponent<Rigidbody>().position, transform.TransformDirection(Vector3.forward), out hit, light.range, layerMask))
+        {
+            Debug.DrawRay(GetComponent<Rigidbody>().position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.black);
+
+            if (hit.collider != null)
+            {
+                //raycastReturn = hit.collider.gameObject.name;
+                //foundObject = GameObject.Find(raycastReturn);
+                foundObject = hit.collider.gameObject.GetComponent<LightUpObject>();
+                foundObject.LightUp();
+            }
+        }
+        else
+        {
+            Debug.DrawRay(GetComponent<Rigidbody>().position, transform.TransformDirection(Vector3.forward) * 1000, Color.black);
+
+            if (foundObject != null)
+            {
+                foundObject.LightDown();
+            }
+        }
+    }
+}
