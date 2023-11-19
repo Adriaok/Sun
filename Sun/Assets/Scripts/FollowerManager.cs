fd591a6b4beccf654862670316a490d152251a68
Iris LC <iris.lazaro@enti.cat>
2023-11-19 14:52:56 +0100

    First part of terrain

--- Sun/Assets/Scripts/FollowerManager.cs
+++ Sun/Assets/Scripts/FollowerManager.cs
@@ -36,6 +36,7 @@ public class FollowerManager : MonoBehaviour
         CheckActionsOnFollowers();
         
         //Test
+        /*
         elapsedTime += Time.deltaTime;
 
         if (elapsedTime > secondsBetweenSpawn && followers.Count < maxFollowers)
@@ -43,6 +44,7 @@ public class FollowerManager : MonoBehaviour
             elapsedTime = 0.0f;
             AddFollower();
         }
+        */
     }
 
     void LockFollower(string id)
@@ -174,7 +176,7 @@ public class FollowerManager : MonoBehaviour
 
     private GameObject InstantiateWhisp(string newID)
     {
-        GameObject newFollower = Instantiate(whispPrefab, new Vector3(Random.Range(-10, 10), 1, 0), Quaternion.identity);
+        GameObject newFollower = Instantiate(whispPrefab, new Vector3(20.0f, 18.8899994f, Random.Range(20, 25)), Quaternion.identity);
         newFollower.GetComponent<SC_Follower>().Init();
         newFollower.GetComponent<SC_Follower>().ID = newID;
         newFollower.layer = 7;
@@ -185,7 +187,7 @@ public class FollowerManager : MonoBehaviour
 
     private GameObject InstantiateLantern(string newID)
     {
-        GameObject newFollower = Instantiate(lanternPrefab, new Vector3(Random.Range(-10, 10), 1, 0), Quaternion.identity);
+        GameObject newFollower = Instantiate(lanternPrefab, new Vector3(20.0f, 18.8899994f, Random.Range(20, 25)), Quaternion.identity);
         newFollower.GetComponent<SC_Follower>().Init();
         newFollower.GetComponent<SC_Follower>().ID = newID;
         newFollower.layer = 7;
@@ -195,7 +197,7 @@ public class FollowerManager : MonoBehaviour
 
     private GameObject InstantiateFlashlight(string newID)
     {
-        GameObject newFollower = Instantiate(flashlightPrefab, new Vector3(Random.Range(-10, 10), 1, 0), Quaternion.identity);
+        GameObject newFollower = Instantiate(flashlightPrefab, new Vector3(20.0f, 18.8899994f, Random.Range(20, 25)), Quaternion.identity);
         newFollower.GetComponent<SC_Follower>().Init();
         newFollower.GetComponent<SC_Follower>().ID = newID;
         newFollower.layer = 7;
