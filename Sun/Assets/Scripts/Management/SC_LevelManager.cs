f3949fa39d1a28125031bed4cfc6368e8bd30f5e
Iris LC <iris.lazaro@enti.cat>
2023-11-26 18:56:34 +0100

    Init flag monument's camera

--- Sun/Assets/Scripts/Management/SC_LevelManager.cs
+++ Sun/Assets/Scripts/Management/SC_LevelManager.cs
@@ -3,8 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+struct Solution
+{
+    List<Dictionary<int, string>> followers;
+}
+
+struct Puzzle
+{
+    const string WHISP_TYPE = "whisp";
+    const string LANTERN_TYPE = "lantern";
+    const string FLASHLIGHT_TYPE = "flashlight";
+
+    Dictionary<int, Solution> solutions;
+    bool completed;
+}
+
+
 public class SC_LevelManager : MonoBehaviour
 {
+    private List<Puzzle> puzzles = new List<Puzzle>();
+
+
     private static SC_LevelManager _instance;
 
     public static SC_LevelManager Instance { get { return _instance; } }
