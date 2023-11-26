4db607b2f0412ad2e8d6bd511f5f707a4ec0435c
Iris LC <iris.lazaro@enti.cat>
2023-11-26 19:18:39 +0100

    Puzzle data structure

--- Sun/Assets/Scripts/Management/SC_LevelManager.cs
+++ Sun/Assets/Scripts/Management/SC_LevelManager.cs
@@ -3,26 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-struct Solution
+public class Solution
 {
-    List<Dictionary<int, string>> followers;
+    private Dictionary<string, int> necessaryFollowers;
+
+    public void InitSolution(Dictionary<string, int> _necessaryFollowers)
+    {
+        necessaryFollowers = new Dictionary<string, int>();
+        necessaryFollowers = _necessaryFollowers;
+    }
 }
 
-struct Puzzle
+public class Puzzle
 {
-    const string WHISP_TYPE = "whisp";
-    const string LANTERN_TYPE = "lantern";
-    const string FLASHLIGHT_TYPE = "flashlight";
+    private Dictionary<int, Solution> solutions;
+    public bool completed;
 
-    Dictionary<int, Solution> solutions;
-    bool completed;
+    public void InitPuzzle(List<Solution> _solutionList)
+    {
+        solutions = new Dictionary<int, Solution>();
+        for(int i = 0; i < _solutionList.Count; i++)
+        {
+            solutions[i] = _solutionList[i];
+        }
+
+        completed = false;
+    }
 }
 
 
 public class SC_LevelManager : MonoBehaviour
 {
-    private List<Puzzle> puzzles = new List<Puzzle>();
+    private const string WHISP_TYPE = "whisp";
+    private const string LANTERN_TYPE = "lantern";
+    private const string FLASHLIGHT_TYPE = "flashlight";
 
+    private List<Puzzle> puzzles = new List<Puzzle>();
 
     private static SC_LevelManager _instance;
 
@@ -39,6 +55,27 @@ public class SC_LevelManager : MonoBehaviour
         {
             _instance = this;
         }
+    }
+
+    private void Start()
+    {
+        InitPuzzles();
+    }
+
+    private void InitPuzzles()
+    {
+        //First puzzle -> make function?
+        Solution solution = new Solution();
+        Dictionary<string, int> necessaryFollowers = new Dictionary<string, int>();
+        necessaryFollowers[WHISP_TYPE] = 1;
+        necessaryFollowers[LANTERN_TYPE] = 1;
+        necessaryFollowers[FLASHLIGHT_TYPE] = 1;
+        solution.InitSolution(necessaryFollowers);
+
+        Puzzle puzzle = new Puzzle();
+        List<Solution> solutionsList = new List<Solution>();
+        solutionsList.Add(solution);
+        puzzle.InitPuzzle(solutionsList);
     }
 
     public void ResetLevel()
