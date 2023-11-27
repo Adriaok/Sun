7bb547bd9814b0872338a4a676a17faca5e6d750
Iris LC <iris.lazaro@enti.cat>
2023-11-27 17:31:10 +0100

    Start working

---
+++ Sun/Assets/Scripts/UI/SC_UI_Onboarding.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_UI_Onboarding : MonoBehaviour
+{
+    private int currentText;
+
+    [SerializeField]
+    private List<GameObject> texts = new List<GameObject>();
+    private GameObject onboardingUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentText = 0;
+        texts[currentText].SetActive(true);
+    }
+
+    public void ChangeToNextText()
+    {
+        texts[currentText].SetActive(false);
+        currentText++;
+
+        if (currentText < texts.Count)
+            texts[currentText].SetActive(true);
+        else
+            onboardingUI.SetActive(false);
+    }
+}
